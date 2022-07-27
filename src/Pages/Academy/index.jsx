7da65a449a188e346c2e1e40a1081785989444f6
souraavsen1 <sourav15-11392@diu.edu.bd>
2022-07-28 00:26:19 +0600

    reafctor: academy page updated

--- src/Pages/Academy/index.jsx
+++ src/Pages/Academy/index.jsx
@@ -21,22 +21,91 @@ const index = () => {
           </div>
         </div>
 
-        <div>
-          <div className='flex items-center'>
-            <img
-              className='w-59 h-32'
-              src='https://i.ibb.co/G2b3dtv/Rectangle-3.png'
-              alt=''
-            />
-            <div className='ml-4'>
-              <h1 className='text-4.5xl leading-12 font-bold font-abhaya mb-0'>
-                he popularity of this megastar fashion house is evident
-              </h1>
-              <p className='font-light text-xs leading-5 font-mulish'>
-                Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla
-                tempor aute reprehenderit ut dolore mollit nisi consequat
-                excepteur ex officia pariatur irure.
-              </p>
+        <div className='flex justify-between items-start pb-6'>
+          <div>
+            <div className='flex mt-6'>
+              <img
+                className='w-59 h-32'
+                src='https://i.ibb.co/519gqHn/Rectangle-2.png'
+                alt=''
+              />
+              <div
+                className='ml-4'
+                style={{
+                  width: "31.125rem",
+                }}
+              >
+                <h1 className='text-4.5xl leading-12 font-bold font-abhaya mb-0'>
+                  Tempor deserunt Sunt Qui
+                </h1>
+                <p className='font-normal text-base leading-6 font-mulish'>
+                  Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla
+                  tempor aute reprehenderit ut dolore mollit nisi consequat
+                  excepteur ex officia pariatur irure.
+                </p>
+              </div>
+            </div>
+
+            <div className='flex mt-6'>
+              <img
+                className='w-59 h-32'
+                src='https://i.ibb.co/519gqHn/Rectangle-2.png'
+                alt=''
+              />
+              <div
+                className='ml-4'
+                style={{
+                  width: "31.125rem",
+                }}
+              >
+                <h1 className='text-4.5xl leading-12 font-bold font-abhaya mb-0'>
+                  Tempor deserunt Sunt Qui
+                </h1>
+                <p className='font-normal text-base leading-6 font-mulish'>
+                  Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla
+                  tempor aute reprehenderit ut dolore mollit nisi consequat
+                  excepteur ex officia pariatur irure.
+                </p>
+              </div>
+            </div>
+
+            <div className='flex mt-6'>
+              <img
+                className='w-59 h-32'
+                src='https://i.ibb.co/519gqHn/Rectangle-2.png'
+                alt=''
+              />
+              <div
+                className='ml-4'
+                style={{
+                  width: "31.125rem",
+                }}
+              >
+                <h1 className='text-4.5xl leading-12 font-bold font-abhaya mb-0'>
+                  Tempor deserunt Sunt Qui
+                </h1>
+                <p className='font-normal text-base leading-6 font-mulish'>
+                  Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla
+                  tempor aute reprehenderit ut dolore mollit nisi consequat
+                  excepteur ex officia pariatur irure.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='mt-6'>
+            <h2 className='text-xl leading-7 font-mulish font-bold'>
+              Category
+            </h2>
+            <div>
+              <p className='text-base leading-6 font-mulish'>Technology</p>
+              <p className='text-base leading-6 font-mulish'>JavaScript</p>
+              <p className='text-base leading-6 font-mulish'>help</p>
+              <p className='text-base leading-6 font-mulish'>Minimalism</p>
+              <p className='text-base leading-6 font-mulish'>Technology</p>
+              <p className='text-base leading-6 font-mulish'>JavaScript</p>
+              <p className='text-base leading-6 font-mulish'>help</p>
+              <p className='text-base leading-6 font-mulish'>Minimalism</p>
+              <p className='text-base leading-6 font-mulish'>Technology</p>
             </div>
           </div>
         </div>
